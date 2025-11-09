dcba1dfd3296fc75cbbb92c0057b931e02262c7a
shamimwebdev <shamim.ct17@gmail.com>
2025-11-09 14:22:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-5xl">hello</p>
+      <p className="text-5xl">hello</p>
+    </>
+  );
+}
+
+export default App;
